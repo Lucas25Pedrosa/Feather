022266115c48a55b392b34b64b9ed79f418cbd8b
Lucas25Pedrosa <41903995+Lucas25Pedrosa@users.noreply.github.com>
2026-08-26 22:12:51 -0300

    Put Lucas first in Feather credits

--- Feather/Views/Settings/About/AboutView.swift
+++ Feather/Views/Settings/About/AboutView.swift
@@ -21,9 +21,9 @@ extension AboutView {
 // MARK: - View
 struct AboutView: View {
 	@State private var _credits: [CreditsModel] = [
+		.init(name: "Lucas", desc: "Developer", github: "Lucas25Pedrosa"),
 		.init(name: "C", desc: "Developer", github: "claration"),
 		.init(name: "Asami", desc: "Developer", github: "Nyasami"),
-		.init(name: "Lucas", desc: "Developer", github: "Lucas25Pedrosa"),
 		.init(name: "Lakhan Lothiyi", desc: "AltStore Repositories", github: "llsc12"),
 	]
 	
